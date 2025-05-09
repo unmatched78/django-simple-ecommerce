76eb3dfb3fc530fe7d9427b4a2cc37da6e5e8a52
villa <marvinavi24@gmail.com>
2025-05-09 17:01:35 +0300

    added some config

--- ecom/urls.py
+++ ecom/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    #serving static, media
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
